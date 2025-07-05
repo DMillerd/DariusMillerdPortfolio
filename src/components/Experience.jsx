f57c28a305c2884b4fd355e9ac653d72714621e4
Darius <dariusmillerd@gmail.com>
2025-07-05 16:41:39 -0500

    added resumeand changed font of navbar

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -12,8 +12,7 @@ return (
         <hr className="gradient-2"></hr>
         </div>
         {/* describe experience more, probably combine it with about page and make a new contact me page */}
-        <p>Languages, tools, possible applications.</p>
-        <p>At my current job, I have been working with a SQL Database, analyzing inventory.</p>
+        <p>At my current job, I have been working with SQL Databases, data collection and analysis, training subordinates.</p>
         <p>Throughout the duration of my education, I've been introduced to many tools, software, and languages that have been helping me progress me career.</p>
     </div>
     );
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,4 +1,6 @@
 //imports
+import DariusMillerdResume from "../assets/Darius-Millerd-Resume.pdf"
+import DariusMillerdResumepng from "../assets/Darius-Millerd-Resume.png"
 
 //main function
 const Resume = () => {
@@ -8,27 +10,14 @@ const Resume = () => {
     return (
         <div>
             <div className="header-container">
-            <h1>This is my Resume</h1>
+            <h1>My Resume</h1>
             <hr className="gradient-2"></hr>
             </div>
             {/* link downloadable copy of resume, also make resume info displayed on this page in a clear and concise manner. */}
             <div>
-                <h2>Education</h2>
-                <li>Austin Community College
-                    Software Development Bootcamp, Computer Software
-                    Engineering, October 2024 - May 2025
-                    Learned how to fully develop and deploy Javascript/HTML/CSS
-                    applications, while using backend technologies such as MongoDB
-                    and SQL, while learning other forms of support/extensions to our
-                    knowledge such as React.JS, Git, Github, Visual Studio Code and many
-                    more.</li>
-                <li> Austin Community College
-                    Associate of Science - AS, Engineering, - December 2023
-                    Focus on Engineering processes and Tech, such as full-stack
-                    development, computer aided design, and electrical engineering.</li>
-            </div>
-            <div>
-                <p>Full resume to be added for download at a later date.</p>
+                <img className="resume-photo" src={DariusMillerdResumepng} alt="My resume" />
+                <h3>Download PDF</h3>
+                <embed src={DariusMillerdResume} width="500px" height="300px" />
             </div>
         </div>
     );
